13b2969baa06fc4e71edab85635d54c56c485269
DLopez6877 <dlopez6877@gmail.com>
2024-10-23 20:49:31 -0700

    add pics

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,20 @@
 import React, {useState} from "react";
 import "./Counters.scss";
 
-import tarmogoyf from "./assets/tarmogoyf.jpg";
-import detritivore from "./assets/detritivore.jpg";
-import mortivore from "./assets/mortivore.jpg";
-import magnivore from "./assets/magnivore.jpg";
-import terravore from "./assets/terravore.webp";
-import fgenesis from "./assets/fgenesis.jpg";
-import lord from "./assets/lord.jpg";
+import Altar from "./assets/Altar_of_the_Goyf.jpg";
+import Barrowgoyf from "./assets/Barrowgoyf.jpg";
+import Detritivore from "./assets/Detritivore.jpg";
+import Formless_Genesis from "./assets/Formless_Genesis.jpg";
+import Lhurgoyf from "./assets/Lhurgoyf.jpg";
+import Lord_of_Extinction from "./assets/Lord_of_Extinction.jpg";
+import Magnivore from "./assets/Magnivore.jpg";
+import Mortivore from "./assets/Mortivore.jpg";
+import Necrogoyf from "./assets/Necrogoyf.jpg";
+import Nethergoyf from "./assets/Nethergoyf.jpg";
+import Polygoyf from "./assets/Polygoyf.jpg";
+import Pyrogoyf from "./assets/Pyrogoyf.jpg";
+import Tarmogoyf from "./assets/Tarmogoyf.jpg";
+import Terravore from "./assets/Terravore.jpg";
 
 // Define the display names and background images for the counters
 const counterDisplayNames = {
@@ -16,18 +23,20 @@ const counterDisplayNames = {
   allCreaturesCount: "All Creatures",
   allLandsCount: "All Lands",
   allSorceriesCount: "All Sorceries",
+  yourTypesCount: "Your Types",
   yourLandsCount: "Your Lands",
   detritivoreCount: "Detritivore",
 };
 
-const counterBackgroundImages = {
-  allCardsCount: lord,
-  allTypesCount: tarmogoyf,
-  allCreaturesCount: mortivore,
-  allLandsCount: terravore,
-  allSorceriesCount: magnivore,
-  yourLandsCount: fgenesis,
-  detritivoreCount: detritivore,
+const counterCardImages = {
+  allCardsCount: [Lord_of_Extinction],
+  allTypesCount: [Altar, Barrowgoyf, Polygoyf, Pyrogoyf, Tarmogoyf],
+  allCreaturesCount: [Lhurgoyf, Mortivore, Necrogoyf],
+  allLandsCount: [Terravore],
+  allSorceriesCount: [Magnivore],
+  yourTypesCount: [Nethergoyf],
+  yourLandsCount: [Formless_Genesis],
+  detritivoreCount: [Detritivore],
 };
 
 const App = () => {
@@ -38,8 +47,9 @@ const App = () => {
     allCreaturesCount: 0,
     allLandsCount: 0,
     allSorceriesCount: 0,
-    detritivoreCount: 0,
+    yourTypesCount: 0,
     yourLandsCount: 0,
+    detritivoreCount: 0,
   };
 
   const [counters, setCounters] = useState(initialCounters);
@@ -60,22 +70,30 @@ const App = () => {
     }));
   };
 
+  // Handle manual input change
+  const handleInputChange = (e, name) => {
+    const value = parseInt(e.target.value, 10);
+    setCounters((prevCounters) => ({
+      ...prevCounters,
+      [name]: isNaN(value) ? 0 : value,
+    }));
+  };
+
   return (
     <div className="counters-container">
       {Object.keys(counters).map((name) => (
         <div className="row" key={name}>
-          <div
-            className="bg"
-            style={{
-              backgroundImage: `url(${counterBackgroundImages[name]})`,
-            }}
-          />
           <div className="row-name">{counterDisplayNames[name]}</div>
+          <div className="cards">
+            {counterCardImages[name].map((cardImage, index) => (
+              <img key={index} src={cardImage} alt={`${counterDisplayNames[name]} card art`} />
+            ))}
+          </div>
           <div className="counter">
             <div className="incrementer" onClick={() => handleDecrement(name)}>
               <div className="minus-icon" />
             </div>
-            <div className="count">{counters[name]}</div>
+            <input type="number" className="count-input" value={counters[name]} onChange={(e) => handleInputChange(e, name)} />
             <div className="incrementer" onClick={() => handleIncrement(name)}>
               +
             </div>
