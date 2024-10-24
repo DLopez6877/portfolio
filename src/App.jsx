f3a1a67bb2499341ed17a6b20f8af6bf4da62caa
DLopez6877 <dlopez6877@gmail.com>
2024-10-24 16:53:18 -0700

    things

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,20 @@
 import React, {useState} from "react";
 import "./Counters.scss";
 
-import Altar from "./assets/Altar_of_the_Goyf.jpg";
-import Barrowgoyf from "./assets/Barrowgoyf.jpg";
-import Detritivore from "./assets/Detritivore.jpg";
-import Formless_Genesis from "./assets/Formless_Genesis.jpg";
-import Lhurgoyf from "./assets/Lhurgoyf.jpg";
-import Lord_of_Extinction from "./assets/Lord_of_Extinction.jpg";
-import Magnivore from "./assets/Magnivore.jpg";
-import Mortivore from "./assets/Mortivore.jpg";
-import Necrogoyf from "./assets/Necrogoyf.jpg";
-import Nethergoyf from "./assets/Nethergoyf.jpg";
-import Polygoyf from "./assets/Polygoyf.jpg";
-import Pyrogoyf from "./assets/Pyrogoyf.jpg";
-import Tarmogoyf from "./assets/Tarmogoyf.jpg";
-import Terravore from "./assets/Terravore.jpg";
+import Altar from "./assets/altarOfTheGoyf.png";
+import Barrowgoyf from "./assets/barrowgoyf.png";
+import Detritivore from "./assets/detritivore.png";
+import Formless_Genesis from "./assets/formlessGenesis.png";
+import Lhurgoyf from "./assets/lhurgoyf.png";
+import Lord_of_Extinction from "./assets/lordOfExtinction.png";
+import Magnivore from "./assets/magnivore.png";
+import Mortivore from "./assets/mortivore.png";
+import Necrogoyf from "./assets/necrogoyf.png";
+import Nethergoyf from "./assets/nethergoyf.png";
+import Polygoyf from "./assets/polygoyf.png";
+import Pyrogoyf from "./assets/pyrogoyf.png";
+import Tarmogoyf from "./assets/tarmogoyf.png";
+import Terravore from "./assets/terravore.png";
 
 // Define the display names and background images for the counters
 const counterDisplayNames = {
@@ -53,6 +53,25 @@ const App = () => {
   };
 
   const [counters, setCounters] = useState(initialCounters);
+  const [lifeCounter, setLifeCounter] = useState(40);
+  const [drawerOpen, setDrawerOpen] = useState(false);
+  const [visibility, setVisibility] = useState(
+    Object.keys(counterCardImages).reduce((acc, key) => {
+      acc[key] = counterCardImages[key].map(() => false); // Default all images to visible
+      return acc;
+    }, {})
+  );
+
+  // Increment life value
+  const handleIncrementLife = (amount) => {
+    setLifeCounter(lifeCounter + 1);
+  };
+
+  // Handle manual life input change
+  const handleLifeInputChange = (e, name) => {
+    const value = parseInt(e.target.value, 10);
+    setLifeCounter(value);
+  };
 
   // Increment counter value
   const handleIncrement = (name) => {
@@ -79,28 +98,78 @@ const App = () => {
     }));
   };
 
+  // Toggle visibility of an image
+  const toggleVisibility = (category, index) => {
+    setVisibility((prevVisibility) => ({
+      ...prevVisibility,
+      [category]: prevVisibility[category].map((v, i) => (i === index ? !v : v)),
+    }));
+    console.log(visibility);
+  };
+
+  // Handle drawer toggle
+  const handleDrawerToggle = () => {
+    setDrawerOpen(!drawerOpen);
+  };
+
   return (
-    <div className="counters-container">
-      {Object.keys(counters).map((name) => (
-        <div className="row" key={name}>
-          <div className="row-name">{counterDisplayNames[name]}</div>
-          <div className="counter">
-            <div className="incrementer" onClick={() => handleDecrement(name)}>
-              <div className="minus-icon" />
-            </div>
-            <input type="number" className="count-input" value={counters[name]} onChange={(e) => handleInputChange(e, name)} />
+    <>
+      <div className="counters-container">
+        <button className="drawer-toggle-button" onClick={handleDrawerToggle}>
+          Menu
+        </button>
+        {drawerOpen && (
+          <div className="drawer">
+            {Object.keys(counterCardImages).map((category) =>
+              counterCardImages[category].map((image, index) => (
+                <div key={`${category}-${index}`} className="drawer-item">
+                  <div className="drawer-row">
+                    <button onClick={() => toggleVisibility(category, index)}>{visibility[category][index] ? "👁️" : "🚫"}</button>
+                    <img src={image} alt="card image" />
+                  </div>
+                </div>
+              ))
+            )}
+          </div>
+        )}
+
+        {/* Life */}
+        <div className="life-counter">
+          <div className="incrementers">
             <div className="incrementer" onClick={() => handleIncrement(name)}>
               +
             </div>
+            <div className="incrementer" onClick={() => handleDecrement(name)}>
+              <div className="minus-icon" />
+            </div>
           </div>
-          <div className="cards">
-            {counterCardImages[name].map((cardImage, index) => (
-              <img key={index} src={cardImage} alt={`${counterDisplayNames[name]} card art`} />
-            ))}
+          <div className="count-wrapper">
+            <input type="number" className="count-input" value={lifeCounter} onChange={(e) => handleLifeInputChange(e)} />
           </div>
         </div>
-      ))}
-    </div>
+
+        {/* Counter and Images */}
+        {Object.keys(counters).map((name) => (
+          <div className="row" key={name} style={{display: visibility[name].includes(true) ? "flex" : "none"}}>
+            <div className="counter">
+              <div className="incrementers">
+                <div className="incrementer" onClick={() => handleIncrement(name)}>
+                  +
+                </div>
+                <div className="incrementer" onClick={() => handleDecrement(name)}>
+                  <div className="minus-icon" />
+                </div>
+              </div>
+              <div className="count-wrapper">
+                <div className="row-name">{counterDisplayNames[name]}</div>
+                <input type="number" className="count-input" value={counters[name]} onChange={(e) => handleInputChange(e, name)} />
+              </div>
+            </div>
+            <div className="cards">{counterCardImages[name].map((cardImage, index) => visibility[name][index] && <img className="card-image" key={index} src={cardImage} alt={`${counterDisplayNames[name]} card art`} />)}</div>
+          </div>
+        ))}
+      </div>
+    </>
   );
 };
 
